0d38dd5fe67626e10223d39763fd667006b6615e
Michele Bonazza <michele@sketchtogether.com>
2019-10-31 13:19:37 +0100

    home screen - highlight labels, add intro and divider

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,7 +4,9 @@ import {
   StatusBar,
   StyleSheet,
   ScrollView,
-  ImageRequireSource
+  ImageRequireSource,
+  Text,
+  View
 } from "react-native";
 import { Card } from "react-native-urbi-ui/components/Card";
 import {
@@ -13,6 +15,9 @@ import {
 } from "react-native-urbi-ui/molecules/card/CardHeader";
 import { VehicleImg } from "react-native-urbi-ui/molecules/img/VehicleImg";
 import { showAlert } from "react-native-urbi-ui/utils/functions";
+import { registeredTextStyle } from "react-native-urbi-ui/utils/textStyles";
+import { colors } from "react-native-urbi-ui/utils/colors";
+import { SectionsDivider } from "react-native-urbi-ui/molecules/SectionsDivider";
 
 type CardInfo = {
   header: CardHeaderProps;
@@ -21,13 +26,16 @@ type CardInfo = {
   description: string;
 };
 
+const body = registeredTextStyle("body", colors.uma, "dealsbody");
+
 const fakeNews: CardInfo[] = [
   {
     header: {
       topLabel: "kickscooter sharing",
       title: "Circ",
       bigLabel: "Free",
-      struckout: "4.99€"
+      struckout: "4.99€",
+      highlightBigLabel: true
     },
     image: require("../../assets/ic_lime_kick.png"),
     logo: require("../../assets/ic_flash.png"),
@@ -38,7 +46,8 @@ const fakeNews: CardInfo[] = [
       topLabel: "Car sharing",
       title: "Miles",
       bigLabel: "Free",
-      struckout: "9.99€"
+      struckout: "9.99€",
+      highlightBigLabel: true
     },
     image: require("../../assets/ic_driveby_a3.png"),
     logo: require("../../assets/ic_driveby.png"),
@@ -49,7 +58,8 @@ const fakeNews: CardInfo[] = [
       topLabel: "Scooter sharing",
       title: "Coup",
       bigLabel: "Free",
-      struckout: "7.99€"
+      struckout: "7.99€",
+      highlightBigLabel: true
     },
     image: require("../../assets/ic_coup_gogoro.png"),
     logo: require("../../assets/ic_coup.png"),
@@ -65,6 +75,17 @@ export const HomeScreen = () => (
     <StatusBar barStyle="dark-content" />
     <SafeAreaView style={styles.wrapper}>
       <ScrollView style={styles.scrollview}>
+        <View style={styles.intro}>
+          <Text style={body}>
+            Unlock other sharing mobility providers with our exclusive deals.
+            Just one tap, you won't need to verify your identity again!
+          </Text>
+        </View>
+        <SectionsDivider
+          label="active promos"
+          labelColor={colors.ughina}
+          backgroundColor={colors.ulisse}
+        />
         {fakeNews.map((c, i) => (
           <Card
             key={`card-${i}`}
@@ -85,5 +106,6 @@ HomeScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   wrapper: { flex: 1, justifyContent: "flex-start" },
-  scrollview: { paddingTop: 20 }
+  scrollview: { paddingTop: 20 },
+  intro: { padding: 24 }
 });
